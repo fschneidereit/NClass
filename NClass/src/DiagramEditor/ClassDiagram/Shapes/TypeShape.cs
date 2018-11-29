a35a30469311f12f36bbbb5022a4b6ae02bbb6a8
Florian Schneidereit <florian.schneidereit@outlook.com>
2018-11-29 22:27:48 +0100

    Adjusted header and member height constants for high DPI.

--- NClass/src/DiagramEditor/ClassDiagram/Shapes/TypeShape.cs
+++ NClass/src/DiagramEditor/ClassDiagram/Shapes/TypeShape.cs
@@ -1,16 +1,17 @@
 // NClass - Free class diagram editor
 // Copyright (C) 2006-2009 Balazs Tihanyi
-// 
-// This program is free software; you can redistribute it and/or modify it under 
-// the terms of the GNU General Public License as published by the Free Software 
+// Copyright (C) 2018 Florian Schneidereit
+//
+// This program is free software; you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
 // Foundation; either version 3 of the License, or (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful, but WITHOUT 
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 // FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License along with 
-// this program; if not, write to the Free Software Foundation, Inc., 
+// You should have received a copy of the GNU General Public License along with
+// this program; if not, write to the Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 using System;
@@ -33,8 +34,8 @@ namespace NClass.DiagramEditor.ClassDiagram.Shapes
 		public const int DefaultHeight = 216;
 		protected const int MarginSize = 8;
 		protected const int IconSpacing = 21;
-		protected const int HeaderHeight = 45;
-		protected const int MemberHeight = 17;
+		protected const int HeaderHeight = 55;
+		protected const int MemberHeight = 18;
 		protected static readonly StringFormat memberFormat;
 
 		static Pen borderPen = new Pen(Color.Black);
